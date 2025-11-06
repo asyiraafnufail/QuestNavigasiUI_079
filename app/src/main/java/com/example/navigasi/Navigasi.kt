3e6f0c48a620b06652eec63fa6b6e395b99b7e81
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-06 14:16:10 +0700

    fix typo

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -10,6 +10,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.navigasi.view.FormIsian
+import com.example.navigasi.view.
+
 
 enum class Navigasi {
     Formulirku,
