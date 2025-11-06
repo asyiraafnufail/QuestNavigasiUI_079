8a3f9e435a6b7c196d8dd19b2351a13060eb0254
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-06 13:13:07 +0700

    menambahkan scaffold

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -1,12 +1,22 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigasi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormIsian(
     JenisK:List<String> = ListOf("Laki-Laki","Perempuan"),
-    OnSubmitBtnClick: () -> Unit) {
-
+    OnSubmitBtnClick: () -> Unit
+){
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar()
+        })
 }
 
 @Composable
