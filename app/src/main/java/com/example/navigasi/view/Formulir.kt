141052ba8b8907ff24bb6df0fc57a83694a13587
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-06 13:48:12 +0700

    menambahkan column pada isi ruang

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -2,24 +2,53 @@
 
 package com.example.navigasi.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasi.R
 
 @Composable
 fun FormIsian(
     JenisK:List<String> = listOf("Laki-Laki","Perempuan"),
     OnSubmitBtnClick: () -> Unit
 ){
-    Scaffold (modifier = Modifier,
+    Scaffold(modifier = Modifier,
         {
-            TopAppBar()
-        })
-}
+            TopAppBar(
+                title = {Text(stringResource(id = R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                colorResource(id = R.color.teal_700))
+            )}
+        ){
+        isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {},
+            )
 
-@Composable
-fun ListOf(x0: String, x1: String) {
-    TODO("Not yet implemented")
+        }
+        }
 }
