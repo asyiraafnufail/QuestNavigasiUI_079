e34c1501cbefbf6ec243a20845da80f6d5cf7752
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-06 13:54:21 +0700

    menambahkan horizontal divider

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -66,6 +66,9 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp), thickness = 1.dp, color = Color.Red)
         }
-        }
+    }
 }
