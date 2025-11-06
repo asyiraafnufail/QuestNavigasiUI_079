c31c4d74faf2f42f499ee21353c18152cdd61ef8
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-06 12:05:00 +0700

    tambahkan composable DataApp dengan parameter NavController

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,6 +1,21 @@
 package com.example.navigasi
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulirku,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold {  }
 }
